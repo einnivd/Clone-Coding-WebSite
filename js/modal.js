2e527130a9f21875ca4c25908338f274127a2ac7
einnivd <yelk0427@gmail.com>
2026-03-31 18:00:27 +0900

    남선알미늄 웹사이트 홈화면 클론 코딩 3일차-javaScript이용

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function () {
+  // gotop 스크롤 이벤트
+  // 변수 선언
+  // function(){} 과 ()=>{} 는 같다.
+  const gotop = this.document.querySelector("#gotop");
+  // if(조건){조건에 부합할 때 하는 행동}
+  // else{조건에 부합하지 않을 때 하는 행동}
+  this.window.addEventListener("scroll", () => {
+    if (this.document.documentElement.scrollTop > 1000) {
+      gotop.style.display = "block";
+    } else {
+      gotop.style.display = "none";
+    }
+  });
+  // gotop버튼 클릭시 위로 스크롤
+  gotop.addEventListener("click", () => {
+    this.window.scrollTo({
+      top: 0,
+      // behavior: "smooth"
+    });
+  });
+});
