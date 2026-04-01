d5eae5d8bd52b500a915678b575403b1104388c2
einnivd <yelk0427@gmail.com>
2026-04-01 10:50:56 +0900

    남선알미늄 클론코딩 4일차-javaScript 사용-modal창 생성

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,15 @@
+// function(){} 과 ()=>{} 는 같다.
 window.addEventListener("load", function () {
+  // 모달창 닫기
+  const modalWrap = this.document.querySelector(".modal-wrap");
+  const modalClose = this.document.querySelector(".modalClose");
+  //  modalClose.addEventListener("이벤트" , function(){행동});
+  // "이벤트"를 할 경우에 {행동}한다.
+  modalClose.addEventListener("click", function () {
+    modalWrap.style.display = "none";
+  });
   // gotop 스크롤 이벤트
   // 변수 선언
-  // function(){} 과 ()=>{} 는 같다.
   const gotop = this.document.querySelector("#gotop");
   // if(조건){조건에 부합할 때 하는 행동}
   // else{조건에 부합하지 않을 때 하는 행동}
